3ec3295fc45cd09e3e9ed4e61917a94c2902bfb5
John Costa <johncosta027@gmail.com>
2021-06-24 19:55:57 +0100

    Added subject functions

--- routers/subjects.router.js
+++ routers/subjects.router.js
@@ -11,23 +11,50 @@ subjects.post(
 	checkJwt,
 	jwtAuthz(['create:subjects'], { customScopeKey: 'permissions' }),
 	async (req, res) => {
-		const { error } = await supabase
-			.from('subjects')
-			.insert([
-				{
-					name: req.body.name,
-					examboard: req.body.examboard,
-					level: req.body.level,
-				},
-			]);
-        
-        if (error != undefined) {
-            res.status(400).send(getDbErrorMessage(error));
-        } else {
+		const { error } = await supabase.from('subjects').insert([
+			{
+				name: req.body.name,
+				examboard: req.body.examboard,
+				level: req.body.level,
+			},
+		]);
+
+		if (error != undefined) {
+			res.status(400).send(getDbErrorMessage(error));
+		} else {
 			res.status(200).send(getSuccessMessage());
-        }
+		}
+	}
+);
+
+subjects.post(
+	'/createpoints',
+	checkJwt,
+	jwtAuthz(['create:subjects'], { customScopeKey: 'permissions' }),
+	async (req, res) => {
 
 	}
 );
+
+subjects.get('/getall', async (req, res) => {
+	const { data, error } = await supabase.from('subjects').select('*');
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		res.status(200).send(data);
+	}
+});
+
+subjects.get('/getalltopics', async (req, res) => {
+	const { data, error } = await supabase
+		.from('topics')
+		.select('*')
+		.match({ subjectid: req.query.subjectid });
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		res.status(200).send(data);
+	}
+});
 
 module.exports = subjects;

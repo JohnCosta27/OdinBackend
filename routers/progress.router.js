217e9733d42454d74f6bbc4632a6b8ca908066b5
John Costa <johncosta027@gmail.com>
2021-06-29 16:48:35 +0100

    Added GET for progress router

--- routers/progress.router.js
+++ routers/progress.router.js
@@ -9,24 +9,42 @@ const {
 	getMissingParametersError,
 } = require('./messages.service');
 const { checkUndefined, checkError } = require('./utilities.service');
-
 const progress = express.Router();
 
+/**
+ * !Very dirty query.
+ */
+progress.get('/get', checkJwt, async (req, res) => {
+	const jwt = jwtDecode(req.headers.authorization.substring(7));
+	const { data, error } = await supabase
+		.from('student_points')
+		.select('*, points (*, topics (*, subjects(*)))')
+		.match({ student_id: jwt.sub });
+
+	console.log(data);
+	console.log(error);
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		res.status(200).send(data);
+	}
+});
+
 progress.post('/add', checkJwt, async (req, res) => {
+	let points = [];
+	const jwt = jwtDecode(req.headers.authorization.substring(7));
+	for (let point of req.body.points) {
+		points.push({ points_id: point, student_id: jwt.sub });
+	}
 
-    let points = [];
-    const jwt = jwtDecode(req.headers.authorization.substring(7));
-    for (let point of req.body.points) {
-        points.push({points_id: point, student_id: jwt.sub});
-    }
-
-    const { data, error } = await supabase.from('student_points').insert(points);
-    if (error != undefined) {
-        res.status(400).send(getDbErrorMessage(error));
-    } else {
-        res.status(200).send(getSuccessMessage());
-    }
-
+	const { data, error } = await supabase
+		.from('student_points')
+		.insert(points);
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		res.status(200).send(getSuccessMessage());
+	}
 });
 
 module.exports = progress;

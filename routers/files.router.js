ef729184f5f90684a159bacf51703caa022afe6d
John Costa <johncosta027@gmail.com>
2021-07-02 23:14:33 +0100

    Finished get content methods

--- routers/files.router.js
+++ routers/files.router.js
@@ -10,11 +10,14 @@ const {
 } = require('./messages.service');
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 const axios = require('axios');
-const bucketName = 'notes'
-const path = `${supabase.storage.url}/object/${bucketName}/`
-const headers = supabase.storage.headers
+const bucketName = 'notes';
+const path = `${supabase.storage.url}/object/${bucketName}/`;
+const getPath = `${supabase.storage.url}/object/authenticated/${bucketName}/`;
+const headers = supabase.storage.headers;
+const FormData = require('form-data');
+const request = require('request');
 
 const files = express.Router();
 
@@ -24,33 +27,55 @@ const files = express.Router();
  */
 
 files.post('/upload', checkJwt, upload.single('note'), async (req, res) => {
-	console.log(req.file);
-	const createNote = await createNoteDb(req.file.originalname)
+	const createNote = await createNoteDb(req.file.originalname);
 	if (createNote.error != undefined) {
 		res.send(getDbErrorMessage(createNote.error));
 	} else {
 		const { data, error } = await supabase
-			.from('topic_notes')
-			.insert([{ noteid: createNote.data[0].id, pointid: req.body.pointid }]);
+			.from('point_notes')
+			.insert([
+				{ noteid: createNote.data[0].id, pointid: req.body.pointid },
+			]);
 		if (error != undefined) {
 			res.send(getDbErrorMessage(error));
 		} else {
-			axios({
-				method: "POST",
-				url: path + req.file.originalname,
-				headers: headers,
-				data: req.file.buffer,
-			  });
+			const formData = {
+				file: {
+					value: req.file.buffer,
+					options: {
+						filename: createNote.data[0].id,
+						contentType: req.file.mimetype
+					}
+				}
+			};
+			const options = {
+				url: path + createNote.data[0].id,
+				headers: supabase.storage.headers,
+				formData: formData
+			}
+			request.post(options);
 			res.send(getSuccessMessage());
 		}
+	}
+});
+
+files.get('/getnotes', checkJwt, async (req, res) => {
+	const { data, error } = await supabase
+		.from('point_notes')
+		.select('*')
+		.match({ pointid: req.query.pointid });
+	if (error != undefined) {
+		res.send(getDbErrorMessage(error));
+	} else {
+		res.send(data);
 	}
 });
 
 async function createNoteDb(filename) {
 	const { data, error } = await supabase
 		.from('notes')
-		.insert([{ url: filename }]);
-	return { data: data, error: error }
+		.insert([{ original_file_name: filename }]);
+	return { data: data, error: error };
 }
 
 module.exports = files;

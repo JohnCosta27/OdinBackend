496ea17060cda2c46e933b5bdb225d4d42beb65b
John Costa <johncosta027@gmail.com>
2021-07-06 15:34:06 +0100

    Added get with progress method

--- routers/progress.router.js
+++ routers/progress.router.js
@@ -27,6 +27,39 @@ progress.get('/get', checkJwt, async (req, res) => {
 	}
 });
 
+progress.get('/getprogressinsubject', checkJwt, async (req, res) => {
+	const jwt = jwtDecode(req.headers.authorization.substring(7));
+	console.log(req.query.studentid);
+	const { data, error } = await supabase.rpc(
+		'get_points_from_subject_student',
+		{ studentid: jwt.sub, givensubjectid: req.query.subjectid }
+	);
+
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		let returnData = [];
+		for (let point of data) {
+
+			let found = false;
+			for (let topic of returnData) {
+				if (topic.topicid == point.topicid) {
+					topic.points.push(point);
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+				returnData.push({topicid: point.topicid, points: [point]});
+			}
+
+		}
+
+		res.status(200).send(returnData);
+	}
+});
+
 progress.post('/add', checkJwt, async (req, res) => {
 	let points = [];
 	const jwt = jwtDecode(req.headers.authorization.substring(7));
@@ -46,11 +79,13 @@ progress.post('/add', checkJwt, async (req, res) => {
 
 progress.post('/addsingular', checkJwt, async (req, res) => {
 	const jwt = jwtDecode(req.headers.authorization.substring(7));
-	const { date, error } = await supabase
-		.from('student_points')
-		.insert([
-			{ points_id: req.body.points_id, student_id: jwt.sub, datetime: req.body.datetime },
-		]);
+	const { date, error } = await supabase.from('student_points').insert([
+		{
+			points_id: req.body.points_id,
+			student_id: jwt.sub,
+			datetime: req.body.datetime,
+		},
+	]);
 	if (error != undefined) {
 		res.status(400).send(getDbErrorMessage(error));
 	} else {

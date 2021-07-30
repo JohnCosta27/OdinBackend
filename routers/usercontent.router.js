a20cc64f7de252463d4872e1d62ccf46bc1636c4
John Costa <johncosta027@gmail.com>
2021-07-30 13:43:10 +0100

    Added get student notes function

--- routers/usercontent.router.js
+++ routers/usercontent.router.js
@@ -26,14 +26,40 @@ usercontent.post('/createnote', checkJwt, async (req, res) => {
 });
 
 const insertTextPointNotes = async (insertData) => {
-	const { data, error } = await supabase.from('text_point_notes').insert(insertData);
-	return {data: data, error: error};
-}
+	const { data, error } = await supabase
+		.from('text_point_notes')
+		.insert(insertData);
+	return { data: data, error: error };
+};
 
 usercontent.get('/getusernotepoint', checkJwt, async (req, res) => {
 	const jwt = jwtDecode(req.headers.authorization.substring(7));
-	const { data, error } = await supabase.rpc('get_user_text_notes', { givenstudentid: jwt.sub, givenpointid: req.query.pointid });
+	const { data, error } = await supabase.rpc('get_user_text_notes', {
+		givenstudentid: jwt.sub,
+		givenpointid: req.query.pointid,
+	});
 	console.log(data);
+	if (error != undefined) {
+		res.status(400).send(getDbErrorMessage(error));
+	} else {
+		res.status(200).send(data);
+	}
+});
+
+usercontent.get('/getusernotes', checkJwt, async (req, res) => {
+	const jwt = jwtDecode(req.headers.authorization.substring(7));
+	let { data, error } = await supabase
+		.from('student_notes')
+		.select('*, text_notes (*)')
+		.match({ studentid: jwt.sub });
+	data.sort(function (a, b) {
+		return (
+			new Date(a.text_notes.datetime) - new Date(b.text_notes.datetime)
+		);
+	});
+	if (data.length > 5) {
+		data = data.slice(0, 5);
+	}
 	if (error != undefined) {
 		res.status(400).send(getDbErrorMessage(error));
 	} else {
